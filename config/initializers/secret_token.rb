72f07711e6a1a6b6b36a1c08a59bd0ec3018d92f
Neve <n.stakanov@tut.by>
2011-08-16 11:35:17 +0300

    upload base

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pr1::Application.config.secret_token = 'e0ef112aa389c4a3ecb7a901ab35a96c4ed6433a01ecaf47c48e43c74228548c750e14860a084ef4e5f109bccc4b279bc66d2fe1afcccf3576b9111dfe2141a3'
